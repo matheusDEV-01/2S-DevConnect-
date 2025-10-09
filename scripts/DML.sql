c450cb9dd4dca0154cff9501ea96f3ecb6cdaa9c
Matheus <matheusgg70jj@gmail.com>
2025-10-09 15:21:46 -0300

    Processo Seletivo (DevConnect)

--- scripts/DML.sql
+++ scripts/DML.sql
@@ -6,6 +6,30 @@ INSERT INTO tb_Usuario (nomeCompleto, nomeDeUsuario, email, senha)
 VALUES 
 ('TTI', 'TTI01', 'TTI0212@gmail.com', '1937');
 
+INSERT INTO tb_Usuario (nomeCompleto, nomeDeUsuario, email, senha)
+VALUES 
+('Lucas Almeida', 'LukDev', 'lucas.almeida@gmail.com', 'lucas456');
+
+INSERT INTO tb_Usuario (nomeCompleto, nomeDeUsuario, email, senha)
+VALUES 
+('Julia Santos', 'JuSantos', 'julia.santos@gmail.com', 'senha789');
+
+INSERT INTO tb_Usuario (nomeCompleto, nomeDeUsuario, email, senha)
+VALUES 
+('Fernanda Costa', 'Fefeh', 'fernanda.costa@gmail.com', 'fefeh321');
+
+INSERT INTO tb_Usuario (nomeCompleto, nomeDeUsuario, email, senha)
+VALUES
+('Carlos Oliveira', 'Carluxo', 'carlos.oliveira@yahoo.com', 'car123');
+
+INSERT INTO tb_Usuario (nomeCompleto, nomeDeUsuario, email, senha)
+VALUES
+('Ana Souza', 'AnaCode', 'ana.souza@hotmail.com', 'abc123');
+
+INSERT INTO tb_Usuario (nomeCompleto, nomeDeUsuario, email, senha)
+VALUES 
+('João Pereira', 'JoaoDev', 'joao.pereira@gmail.com', '12345');
+
 INSERT INTO tb_Usuario (nomeCompleto, nomeDeUsuario, email, senha, fotoPerfilUrl)
 VALUES 
 ('Matheus', 'opalaale', 'alexia@email.com', 'senai@134', NULL);
@@ -24,10 +48,37 @@ VALUES
 
 INSERT INTO tb_Seguidor (id_Usuario_Seguir, id_Usuario_Seguido)
 VALUES 
-(1, 2);
+(3, 1);
+
+INSERT INTO tb_Seguidor (id_Usuario_Seguir, id_Usuario_Seguido)
+VALUES 
+(8, 9); 
+
+INSERT INTO tb_Seguidor (id_Usuario_Seguir, id_Usuario_Seguido)
+VALUES 
+(10, 11); 
+
+INSERT INTO tb_Seguidor (id_Usuario_Seguir, id_Usuario_Seguido)
+VALUES 
+(12, 13); 
+
+INSERT INTO tb_Seguidor (id_Usuario_Seguir, id_Usuario_Seguido)
+VALUES 
+(10, 3); 
+
+INSERT INTO tb_Seguidor (id_Usuario_Seguir, id_Usuario_Seguido)
+VALUES 
+(11, 1);
+
+INSERT INTO tb_Seguidor (id_Usuario_Seguir, id_Usuario_Seguido)
+VALUES 
+(8, 1);  
 
     SELECT * FROM tb_Usuario;
 	SELECT * FROM tb_Publicacao;
 	SELECT * FROM tb_Curtida;
 	SELECT * FROM tb_Comentarios;
 	SELECT * FROM tb_Seguidor;
+
+	ALTER TABLE tb_curtida
+    ADD UNIQUE(id_usuario, id_publicacao)
--- scripts/DQL.sql
+++ scripts/DQL.sql
@@ -12,4 +12,63 @@ SELECT * FROM tb_Comentario;
 
 SELECT * FROM tb_seguidor;
 
-	
+
+--Exibir todas as publicações com descrição, caminho da imagem e nome do usuário
+SELECT 
+    tb_Publicacao.descricao,
+    tb_Publicacao.imagemUrl,
+    tb_Usuario.nomeDeUsuario
+FROM tb_Publicacao 
+LEFT JOIN tb_Usuario  ON tb_Publicacao .id_usuario = tb_Usuario.id;
+
+--Exibir todos os usuários que não fizeram publicação
+SELECT 
+    tb_Usuario.nomeDeUsuario
+FROM tb_Usuario 
+LEFT JOIN tb_Publicacao  ON tb_Usuario.id = tb_Publicacao.id_Usuario
+WHERE tb_Publicacao.id IS NULL;
+
+--Exibir todos os usuários que não fizeram reações
+SELECT 
+  tb_Usuario.nomeDeUsuario
+FROM tb_Usuario 
+LEFT JOIN tb_Curtida  ON tb_Usuario.id = tb_Curtida.id
+WHERE tb_Curtida.id_Publicacao IS NULL;
+
+--Exibir todos os comentários com o nome e o texto de uma respectiva publicação
+SELECT 
+    tb_Usuario.nomeDeUsuario,
+    tb_Comentarios.texto,
+    tb_Publicacao.descricao AS Publicacao
+FROM tb_Comentarios 
+LEFT JOIN tb_Usuario  ON tb_Comentarios.id_Usuario = tb_Usuario.id
+RIGHT JOIN tb_Publicacao  ON tb_Comentarios.id_Publicacao = tb_Publicacao.id;
+
+--Exibir todos os nomes dos seguidores (quem segue e quem é seguido)
+SELECT 
+    seguidor.nomeCompleto AS Nome_Seguidor,
+    seguido.nomeCompleto AS Nome_Seguido
+FROM tb_Seguidor 
+LEFT JOIN tb_Usuario seguidor ON tb_Seguidor.id_Usuario_Seguido = seguidor.id
+RIGHT JOIN tb_Usuario seguido ON tb_Seguidor.id_Usuario_Seguido = seguido.id;
+
+
+--Exiba quando seguidores possui um respectivo usuário
+SELECT 
+    tb_Usuario.nomeCompleto AS Usuario,
+    COUNT(tb_Seguidor.id_Usuario_Seguido) AS Quantidade_Seguidores
+FROM tb_Usuario 
+LEFT JOIN tb_Seguidor ON tb_Usuario.id = tb_Seguidor.id_Usuario_Seguido
+GROUP BY tb_Usuario.nomeCompleto;
+
+--Exibir a quantidade de curtidas de uma respectiva publicação
+SELECT 
+    tb_Publicacao.descricao AS Publicacao,
+    COUNT(tb_Curtida.id_Usuario) AS Quantidade_Curtidas
+FROM tb_Curtida 
+LEFT JOIN tb_Publicacao  ON tb_Curtida.id_Publicacao = tb_Publicacao.id
+GROUP BY tb_Publicacao.descricao;
+
+
+
+
