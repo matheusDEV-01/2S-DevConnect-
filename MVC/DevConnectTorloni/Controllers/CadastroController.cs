e625f0c847b17201fb701e25c6aa8615ae14d5c4
Matheus <matheusgg70jj@gmail.com>
2025-12-02 16:50:41 -0300

    Atividade do DevConnect de -MVC

---
+++ MVC/DevConnectTorloni/Controllers/CadastroController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DevConnectTorloni.Controllers
+{
+    
+    public class CadastroController : Controller
+    {
+        private readonly ILogger<CadastroController> _logger;
+
+        public CadastroController(ILogger<CadastroController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
---
+++ MVC/DevConnectTorloni/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DevConnectTorloni.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
